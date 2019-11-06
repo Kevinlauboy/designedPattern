9b4523ef6d081d164b58cfb5aacfed8ce8af9d8a
Kevinlauboy <mingwanlau@126.com>
2019-11-06 15:44:23 +0800

    外观模式

---
+++ Facade/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Facade.facade;
+namespace Facade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FacadePlat facade = new FacadePlat();
+        }
+    }
+}
---
+++ Facade/facade/FacadePlat.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Facade.facade
+{
+    class FacadePlat
+    {
+        SubSystemOne one;
+        SubSystemTwo two;
+        SubSystemThree three;
+        SubSystemFour four;
+        public FacadePlat()
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+        }
+
+        public void MethodA()
+        {
+            Console.WriteLine("\n 方法组一");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+        public void MethodB()
+        {
+            Console.WriteLine("\n 方法组二");
+            one.MethodOne();
+            three.MethodThree();
+            four.MethodFour();
+        }
+    }
+}
