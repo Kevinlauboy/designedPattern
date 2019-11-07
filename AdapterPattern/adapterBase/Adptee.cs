9e13b747701464be63e18cf86d2bbe289066ce48
Kevinlauboy <mingwanlau@126.com>
2019-11-07 17:16:47 +0800

    适配器模式

---
+++ AdapterPattern/adapterBase/Adptee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterPattern.adapterBase
+{
+    class Adptee
+    {
+        public void SpecialRequest()
+        {
+            Console.WriteLine("特殊请求");
+        }
+    }
+}
---
+++ AdapterPattern/adapterBase/Adpter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdapterPattern.adapterBase
+{
+    class Adpter : Target
+    {
+        private Adptee adptee =new Adptee();
+        public override void Request()
+        {
+            adptee.SpecialRequest();
+        }
+    }
+}
