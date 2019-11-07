29620eefa55198a11d78cc45fd44eccd228eb101
Kevinlauboy <mingwanlau@126.com>
2019-11-08 07:08:53 +0800

    备忘录模式

---
+++ CompositePattern/compositePattern/Composite.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompositePattern.compositePattern
+{
+    class Composite:Component
+    {
+        public override void Add(Component c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove(Component c)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Display(int depth)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CompositePattern/compositePattern/Leaf.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CompositePattern.compositePattern
+{
+    class Leaf : Component
+    {
+        public override void Add(Component c)
+        {
+            Console.WriteLine("cannot add to a leaf");
+        }
+
+        public override void Remove(Component c)
+        {
+            Console.WriteLine("cannot remove from  a leaf");
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+        }
+    }
+}
