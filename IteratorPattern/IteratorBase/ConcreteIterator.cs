ebd570c1b5b4d83a90b4b2bea9d13e0b3413808e
Kevinlauboy <mingwanlau@126.com>
2019-11-08 10:36:05 +0800

    迭代器模式

---
+++ IteratorPattern/IteratorBase/ConcreteIterator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IteratorPattern.IteratorBase
+{
+    
+    class ConcreteIterator : Iterator
+    {
+        private int current = 0;
+        private ConcreteAggregate aggregate;
+
+        public ConcreteIterator(ConcreteAggregate aggregate)
+        {
+            this.aggregate = aggregate;
+        }
+        public override object First()
+        {
+            return aggregate[0];
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+            current++;
+
+            if (current < aggregate.Count)
+            {
+                ret = aggregate[current];
+            }
+
+            return ret;
+        }
+
+        public override bool IsDone()
+        {
+            return current >= aggregate.Count ? true : false;
+        }
+
+        public override object CurrentItem()
+        {
+            return aggregate[current];
+        }
+    }
+}
---
+++ IteratorPattern/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using IteratorPattern.IteratorBase;
+namespace IteratorPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            ConcreteAggregate a = new ConcreteAggregate();
+
+            a[0] = "大鸟";
+            a[1] = "小菜";
+            a[2] = "行李";
+            a[3] = "老外";
+            a[4] = "公交内部员工";
+            a[5] = "小偷";
+
+            Iterator i = new ConcreteIterator(a);
+            //Iterator i = new ConcreteIteratorDesc(a);
+            object item = i.First();
+            while (!i.IsDone())
+            {
+                Console.WriteLine("{0} 请买车票!", i.CurrentItem());
+                i.Next();
+            }
+            */
+
+            IList<string> a = new List<string>();
+            a.Add("大鸟");
+            a.Add("小菜");
+            a.Add("行李");
+            a.Add("老外");
+            a.Add("公交内部员工");
+            a.Add("小偷");
+
+            foreach (string item in a)
+            {
+                Console.WriteLine("{0} 请买车票!", item);
+            }
+
+            IEnumerator<string> e = a.GetEnumerator();
+            while (e.MoveNext())
+            {
+                Console.WriteLine("{0} 请买车票!", e.Current);
+
+            }
+            Console.Read();
+        }
+    }
+}
