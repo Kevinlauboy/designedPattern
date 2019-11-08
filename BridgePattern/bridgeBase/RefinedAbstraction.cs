c3e3bf5dd6af3c2f12d9f6f28ef780bbc2421e71
Kevinlauboy <mingwanlau@126.com>
2019-11-08 12:04:29 +0800

    桥接模式

---
+++ BridgePattern/bridgeBase/RefinedAbstraction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BridgePattern.bridgeBase
+{
+    class RefinedAbstraction : Abstraction
+    {
+        public override void Operation()
+        {
+            implementor.Operation();
+        }
+    }
+}
