5bfbced60d9314d7d0953b231c1e8729c1f599a5
Kevinlauboy <mingwanlau@126.com>
2019-11-12 09:46:52 +0800

    享元模式

---
+++ fyweightPattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using fyweightPattern.fyweightBase;
+namespace fyweightPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int extrinsicstate = 22;
+            FlyweightFactory f = new FlyweightFactory();
+            Flyweight fx = f.GetFlyweight("X");
+            fx.Operation(--extrinsicstate);
+
+            Flyweight fy = f.GetFlyweight("Y");
+            fy.Operation(--extrinsicstate);
+
+            Flyweight fz = f.GetFlyweight("Z");
+            fz.Operation(--extrinsicstate);
+            UnsharedConcreteFlyweight uf = new UnsharedConcreteFlyweight();
+            uf.Operation(--extrinsicstate);
+            Console.Read();
+
+        }
+    }
+}
---
+++ fyweightPattern/fyweightBase/UnsharedConcreteFlyweight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fyweightPattern.fyweightBase
+{
+    class UnsharedConcreteFlyweight :Flyweight
+    {
+
+        public override void Operation(int extrinsicstate)
+        {
+            Console.WriteLine("不共享具体Flvweight:" + extrinsicstate);
+        }
+    }
+}
